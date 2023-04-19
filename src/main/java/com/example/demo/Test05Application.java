b51c7f05041a25c149f98fb0800f81b6db3bf21c
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2023-04-19 20:17:18 +0900

    Update Test05Application.java

--- src/main/java/com/example/demo/Test05Application.java
+++ src/main/java/com/example/demo/Test05Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test05Application {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(Test05Application.class, args);
 	}
 
